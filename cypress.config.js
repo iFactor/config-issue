dfab04a1eefadba0bb795a4fad60392aa7bc3ebe
Don <don.welsh@kubra.com>
2023-04-30 14:50:00 -0700

    update files

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'nhehz6',
   watchForFileChanges: false,
   videoUploadOnPasses: false,
   experimentalStudio: true,
